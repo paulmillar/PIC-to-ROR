c6a1148071d889bbaf845018aa49a095feb1af17
Paul Millar <paul.millar@desy.de>
2021-12-15 10:42:58 +0100

    Split wikidata query into a print function and a support module

---
+++ wikidata.py
@@ -0,0 +1,41 @@
+# pip install sparqlwrapper
+# https://rdflib.github.io/sparqlwrapper/
+
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+query = """SELECT DISTINCT ?vat ?rorId WHERE {
+  ?org (p:P3608/ps:P3608) ?vat;
+    (p:P6782/ps:P6782) ?rorId.
+}
+"""
+
+
+def get_results(endpoint_url, query):
+    """Query wikidata to discover all organisations for which wikidata
+    knows both the EU VAT number and the ROR identifier.  The returned
+    value is a dict with keys VAT number and the values are ROR
+    identifiers.
+    """
+    user_agent = "PIC-to-ROR/0.1 (https://github.com/paulmillar/PIC-to-ROR) Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+def vat_to_ror():
+    print("Querying WikiData to discover EU VAT numbers")
+    results = get_results(endpoint_url, query)
+
+    print("Found {} ROR organisation with their corresponding EU VAT number".format(len(results["results"]["bindings"])))
+
+    vatToRor={}
+    for result in results["results"]["bindings"]:
+        vat=result['vat']['value']
+        ror="https://ror.org/{}".format(result['rorId']['value'])
+        vatToRor[vat]=ror
+
+    return vatToRor
+
